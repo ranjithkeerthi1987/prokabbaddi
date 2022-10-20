e95b4e1bd63dce47ac36cc81ce809e02fb1256b7
ranjithkeerthi <ranjithkeerthi1987>
2022-10-20 23:27:37 +0530

    code founded right now

---
+++ src/main/java/gamemania/App.java
@@ -0,0 +1,13 @@
+package gamemania;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
